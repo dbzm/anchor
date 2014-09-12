be9ecb8ccd1b130b4e454c9f9f743722e2f2df81
dbzm <dmaier@predicta.io>
2014-09-12 15:37:06 -0700

    fix prezto deploy

---
+++ prezto.sh
@@ -0,0 +1,6 @@
+ZDOTDIR=${HOME}/.homesick/repos/anchor/garden
+
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR}"/.zprezto/runcoms/^README.md(.N); do
+  ln -s "$rcfile" "${ZDOTDIR:HOME}/.${rcfile:t}"
+done
