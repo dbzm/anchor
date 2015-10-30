b987be836b65c3c2377023d422971a4b597d758d
dbzm <dovave.maier@gmail.com>
2015-10-30 09:36:53 -0700

    Stop using submodules
    This is insance.  Imma just fork the repo and create a stable branch that does
    what I want.  Bam!

--- prezto.sh
+++ prezto.sh
@@ -1,4 +1,6 @@
-ln -sf ${PWD}/garden/.zprezto $HOME/
+ZPREZTO=${PWD}/garden/.zprezto
+git clone --recursive -b pegged git@github.com:dbzm/prezto.git ${ZPREZTO}
+ln -sf ${ZPREZTO} $HOME/
 setopt EXTENDED_GLOB
 for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
   ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
